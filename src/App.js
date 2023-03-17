2a96af6ace8be485a26fe18798be40cec16ae687
jayStackk <jmagpantay@codestack.co>
2023-03-17 15:40:15 -0700

    peer review

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import AppContainer from "./components/AppContainer";
 import { ThemeProvider } from "./context/themeSwitch";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// peer review by [Shaun]
+// The app looks good, seems like it's getting there, just didnt't quite finish. Works as intended. Revise it when you have the time, But overall good effort on the project.
+
 function App() {
   return (
     <ThemeProvider>
